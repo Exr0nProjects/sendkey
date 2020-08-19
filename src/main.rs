0875b1e1597dfc25b1607bccbfec5f9016524262
Exr0n <spotyie@gmail.com>
2020-08-19 15:55:55 -0700

    I give up on writing a tool
    I guess this'll just be a binary to release the control key :/

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,23 @@
 extern crate autopilot;
 
-use autopilot::key::{KeyCodeConvertible, toggle, Character};
-use structopt::StructOpt;
-
-#[derive(StructOpt, Debug)]
-enum Cli {
-    Down { code: char },
-    Up { code: char }
-}
+use autopilot::key::{KeyCode, Code, toggle, Character};
+//use structopt::StructOpt;
+//
+//#[derive(StructOpt, Debug)]
+//enum Cli {
+//    Down { code: char },
+//    Up { code: char }
+//}
+//
+//fn main() {
+//    let args = Cli::from_args();
+//    println!("args: {:?}", args);
+//    match args {
+//        Cli::Up{code} => toggle(&Character(code), false, &[], 0),
+//        Cli::Down{code} => toggle(&Character(code), true, &[], 0)
+//    }
+//}
 
 fn main() {
-    let args = Cli::from_args();
-    println!("args: {:?}", args);
-    match args {
-        Cli::Up{code} => toggle(&Character(code), false, &[], 0),
-        Cli::Down{code} => toggle(&Character(code), true, &[], 0)
-    }
+    toggle(&Code(KeyCode::Control), false, &[], 0);
 }
