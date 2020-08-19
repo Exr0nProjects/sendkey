7085c63cde2dd0d1ae34ca1820a8a302f374128c
Exr0n <spotyie@gmail.com>
2020-08-19 15:53:09 -0700

    well it works for characters but thats not the important thing

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,17 @@ extern crate autopilot;
 use autopilot::key::{KeyCodeConvertible, toggle, Character};
 use structopt::StructOpt;
 
-#[derive(StructOpt)]
+#[derive(StructOpt, Debug)]
 enum Cli {
-    // down?
-    Down { code: KeyCodeConvertible },
-    Up { code: KeyCodeConvertible }
-    //#[structopt(long, parse(try_from_str = parse_bool))]
-    //down: bool,
-    //code: u32
+    Down { code: char },
+    Up { code: char }
 }
 
 fn main() {
     let args = Cli::from_args();
+    println!("args: {:?}", args);
     match args {
-        Cli::Up{code} => toggle(&Character('a'), false, &[], 0),
-        Cli::Down{code} => toggle(&Character('a'), true, &[], 0)
+        Cli::Up{code} => toggle(&Character(code), false, &[], 0),
+        Cli::Down{code} => toggle(&Character(code), true, &[], 0)
     }
 }
